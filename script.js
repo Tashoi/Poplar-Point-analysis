0057a8b8e8a7daf82cba6ea4d5208956ea87ef4f
Tashoi <seonakaye20@gmail.com>
2026-05-08 10:32:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 const ctx = document.getElementById('rlvChart');
 
 new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Project Connect', 'Tysons', 'Poplar Point'],
-        datasets: [{
-            label: 'Residual Land Value ($M)',
-            data: [44.4, 39.7, 5.5],
-            backgroundColor: [
-                'green',
-                'orange',
-                'red'
-            ]
-        }]
+        labels: [
+            'Base',
+            'Rent -5%',
+            'Hard Cost +10%',
+            'Cap Rate +100bps'
+        ],
+        datasets: [
+            {
+                label: 'Project Connect',
+                data: [47.6, 44.4, 40.1, 30.4],
+                borderColor: 'green',
+                fill: false
+            },
+            {
+                label: 'Tysons Central',
+                data: [24.3, 6.5, 6.7, -2.5],
+                borderColor: 'orange',
+                fill: false
+            },
+            {
+                label: 'Poplar Point',
+                data: [5.5, -12.9, -12.4, -21.6],
+                borderColor: 'red',
+                fill: false
+            }
+        ]
     },
     options: {
         responsive: true,
         plugins: {
             legend: {
-                display: false
+                display: true
             }
         }
     }
